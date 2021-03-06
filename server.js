325235943ff0561214d3976ac5de473a1e8adf01
Korey <smithrkorey@gmail.com>
2021-03-05 21:59:11 -0700

    edits

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/budget-tracker-ks-2021',
+  process.env.MONGODB_URI || 'mongodb://localhost/budget',
   {
   useNewUrlParser: true,
   useUnifiedTopology: true,
